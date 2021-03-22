9453aab7f45c9890e61544ffc4f0a4387a7af842
Sujith Quintelier <sujith.quintelier@gmail.com>
2021-03-22 15:18:18 +0100

    fill id and date

---
+++ challenges/challenge3/starter/api/shared/catalog-data.js
@@ -0,0 +1,26 @@
+const { executeSQL, insertSQL } = require('./sql-utils');
+const TYPES = require('tedious').TYPES;
+
+getCatalog = async function () {
+  const query = `SELECT Id, Name, Description, ImageUrl FROM Icecreams`;
+  return await executeSQL(query);
+}
+
+postOrder = async function (order) {
+
+  const query = `INSERT Orders ([Id], [User], [Date], [IcecreamId], [Status], [DriverId], [FullAddress], [LastPosition]) OUTPUT INSERTED.Id VALUES (@Id, @User, @Date, @IcecreamId, @Status, @DriverId, @FullAddress, @LastPosition)`;
+  const params = [];
+  // Add Param Values
+  params.push({ Name: 'Id', Type: TYPES.NVarChar, Value: order.Id });
+  params.push({ Name: 'User', Type: TYPES.NVarChar, Value: order.User });
+  params.push({ Name: 'Date', Type: TYPES.NVarChar, Value: order.Date });
+  params.push({ Name: 'Status', Type: TYPES.NVarChar, Value: order.Status });
+  params.push({ Name: 'IcecreamId', Type: TYPES.Int, Value: order.IcecreamId });
+  params.push({ Name: 'FullAddress', Type: TYPES.NVarChar, Value: order.FullAddress });
+  params.push({ Name: 'LastPosition', Type: TYPES.NVarChar, Value: null });
+  params.push({ Name: 'DriverId', Type: TYPES.Int, Value: null });
+
+  return await insertSQL(query, params);
+}
+
+module.exports = { getCatalog, postOrder };
