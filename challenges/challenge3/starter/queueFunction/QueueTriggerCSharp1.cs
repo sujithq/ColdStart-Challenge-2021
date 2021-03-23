16dbe306d403eecbb519ac460274a9e84b15c731
Sujith Quintelier <sujith.quintelier@gmail.com>
2021-03-23 13:02:05 +0100

    Part 1

---
+++ challenges/challenge3/starter/queueFunction/QueueTriggerCSharp1.cs
@@ -0,0 +1,112 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class QueueTriggerCSharp1
+    {
+        [Function("QueueTriggerCSharp1")]
+        [CosmosDBOutput(databaseName: "coldstartchallenge2021",
+                    collectionName: "orders",
+                    ConnectionStringSetting = "CosmosDBConnection")]
+        public static OrderDB Run([QueueTrigger("preorder", Connection = "sujithqcschallenge2021_STORAGE")] string myQueueItem,
+                FunctionContext context)
+        {
+            var logger = context.GetLogger("QueueTriggerCSharp1");
+
+            logger.LogInformation($"C# Queue trigger function processing: {myQueueItem}");
+
+            logger.LogInformation($"C# Queue trigger function Deserialize: {myQueueItem}");
+
+            var orderIn = JsonConvert.DeserializeObject<Order>(myQueueItem);
+
+            logger.LogInformation($"C# Queue trigger function Composing: {myQueueItem}");
+
+            var document = new OrderDB
+            {
+                Id = orderIn.Id,
+                User = orderIn.User,
+                Date = orderIn.Date,
+                Icecream = new Icream()
+                {
+                    IcecreamId = orderIn.IcecreamId,
+                    Name = null,
+                    Description = null,
+                    ImageUrl = null
+                },
+                Status = "Accepted",
+                Driver = new Driver()
+                {
+                    DriverId = Guid.Empty,
+                    Name = null,
+                    ImageUrl = null
+                },
+                FullAddress = orderIn.FullAddress,
+                DeliveryPosition = null,
+                LastPosition = null
+            };
+            logger.LogInformation($"C# Queue trigger function Processed: {document}");
+
+            return document;
+        }
+    }
+
+    public class Driver
+    {
+        [JsonProperty(PropertyName = "driverId")]
+        public Guid DriverId { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "imageUrl")]
+        public string ImageUrl { get; set; }
+    }
+
+    public class Icream
+    {
+        [JsonProperty(PropertyName = "icecreamId")]
+        public int IcecreamId { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+        [JsonProperty(PropertyName = "imageUrl")]
+        public string ImageUrl { get; set; }
+    }
+
+    public class OrderDB
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid Id { get; set; }
+        [JsonProperty(PropertyName = "user")]
+        public string User { get; set; }
+        [JsonProperty(PropertyName = "date")]
+        public DateTime Date { get; set; }
+        [JsonProperty(PropertyName = "icecream")]
+        public Icream Icecream { get; set; }
+        [JsonProperty(PropertyName = "status")]
+        public string Status { get; set; }
+        [JsonProperty(PropertyName = "driver")]
+        public Driver Driver { get; set; }
+        [JsonProperty(PropertyName = "fullAddress")]
+        public string FullAddress { get; set; }
+        [JsonProperty(PropertyName = "deliveryPosition")]
+        public string DeliveryPosition { get; set; }
+        [JsonProperty(PropertyName = "lastPosition")]
+        public string LastPosition { get; set; }
+
+    }
+
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public string User { get; set; }
+        public int IcecreamId { get; set; }
+        public string Status { get; set; }
+        public string DriverId { get; set; }
+        public string FullAddress { get; set; }
+        public string LastPosition { get; set; }
+    }
+}
